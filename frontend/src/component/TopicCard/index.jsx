33f79597e6787930d31bf6a3d30f5a78344203b6
Luckzman <lumpsey@gmail.com>
2019-12-12 00:28:11 +0100

    create resuable topic card and add it to topic category session

---
+++ frontend/src/component/TopicCard/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './TopicCard.scss';
+
+const TopicCard = () => (
+  <div className="card">
+    <h1>Motion</h1>
+    <p>I want to know more about the types of motion, any comment is welcome and how things are done in relation to what we are....</p>
+    <div className="card-content">
+      <img
+        alt="user profile"
+        className="card-avatar"
+        src="https://res.cloudinary.com/dx0nauane/image/upload/v1576065923/avatar.jpg" />
+      <div className="card-info">
+        <div className="card-details">
+          <h2>Joyce Dennis</h2>
+          <p>Mofessor group of school</p>
+        </div>
+        <p>24 views</p>
+      </div>
+    </div>
+  </div>
+)
+
+export default TopicCard;
